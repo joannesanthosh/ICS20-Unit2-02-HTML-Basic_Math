7fafb7e1585ee6dddbdf61f646ec24ec35e7fe62
joannesanthosh <99355156+joannesanthosh@users.noreply.github.com>
2022-03-10 18:30:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  // this function does basic math
+  
+document.getElementById('multiply-math').innerHTML ='<p> 5 × 3 = ' + (5 * 3) + '</p>'
+document.getElementById('add-math').innerHTML ='<p> 5 + 3 + 5 + 3= ' + (5 + 3 + 5 + 3) + '</p>'
+}
